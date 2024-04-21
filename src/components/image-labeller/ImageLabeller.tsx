26ace871e9413cd8bf7d56c047638076239145ba
Oliver Broomhall <obroomhall@gmail.com>
2024-04-21 10:52:12 +0100

    Add large crosshairs to image layer

--- src/components/image-labeller/ImageLabeller.tsx
+++ src/components/image-labeller/ImageLabeller.tsx
@@ -3,31 +3,62 @@ import useImage from "use-image";
 import styles from "./ImageLabeller.module.css";
 import useImageContainer from "./UseImageContainer.ts";
 import { Dimensions } from "./Dimensions.ts";
-import { Layer, Image, Stage } from "react-konva";
+import { Layer, Image, Stage, Line } from "react-konva";
 import Konva from "konva";
+import { useState } from "react";
+import { Position } from "../shapes/Position.ts";
+
+interface ImageLayerCrosshairsProps {
+  mousePosition: Position;
+  dimensions: Dimensions;
+}
+
+function ImageLayerCrosshairs({
+  mousePosition: { x, y },
+  dimensions: { height, width },
+}: ImageLayerCrosshairsProps) {
+  return (
+    <>
+      <Line points={[0, y, width, y]} stroke="black" strokeWidth={1} />
+      <Line points={[x, 0, x, height]} stroke="black" strokeWidth={1} />
+    </>
+  );
+}
 
 interface ImageLayerProps {
   image: HTMLImageElement;
   dimensions: Dimensions;
   onMouseDown: (e: Konva.KonvaEventObject<MouseEvent>) => void;
-  onMouseEnter: (e: Konva.KonvaEventObject<MouseEvent>) => void;
+  mousePosition: Position | null;
+  setMousePosition: (position: Position | null) => void;
 }
 
 function ImageLayer({
   image,
   dimensions,
   onMouseDown,
-  onMouseEnter,
+  mousePosition,
+  setMousePosition,
 }: ImageLayerProps) {
   return (
-    <Layer>
+    <Layer
+      onMouseDown={onMouseDown}
+      onMouseMove={({ evt }) =>
+        setMousePosition({ x: evt.offsetX, y: evt.offsetY })
+      }
+      onMouseLeave={() => setMousePosition(null)}
+    >
       <Image
         image={image}
         width={dimensions.width}
         height={dimensions.height}
-        onMouseDown={onMouseDown}
-        onMouseEnter={onMouseEnter}
       />
+      {mousePosition && (
+        <ImageLayerCrosshairs
+          mousePosition={mousePosition}
+          dimensions={dimensions}
+        />
+      )}
     </Layer>
   );
 }
@@ -39,14 +70,22 @@ interface ImageLabellerWindowProps {
 
 function ImageLabellerWindow({ image, rectangles }: ImageLabellerWindowProps) {
   const { ref, dimensions, scale } = useImageContainer(image);
+  const [mousePosition, setMousePosition] = useState<Position | null>(null);
+  const mouseOverImage = !!mousePosition;
+  const cursor = mouseOverImage ? "none" : "default";
   return (
     <div className={styles.stageContainer} ref={ref}>
-      <Stage width={dimensions.width} height={dimensions.height}>
+      <Stage
+        width={dimensions.width}
+        height={dimensions.height}
+        style={{ cursor: cursor }}
+      >
         <ImageLayer
           image={image}
           dimensions={dimensions}
           onMouseDown={() => {}}
-          onMouseEnter={() => {}}
+          mousePosition={mousePosition}
+          setMousePosition={setMousePosition}
         />
       </Stage>
     </div>
