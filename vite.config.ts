e4525371cbe3fd2f31c2c040ac539d7907dd4abf
Oliver Broomhall <obroomhall@gmail.com>
2024-05-12 23:08:33 +0100

    Add certs in development mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import fs from "fs";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  ...(mode === "development" && {
+    server: {
+      https: {
+        key: fs.readFileSync("../.certs/localhost/key.pem"),
+        cert: fs.readFileSync("../.certs/localhost/cert.pem"),
+      },
+    },
+  }),
   plugins: [react()],
-});
+}));
