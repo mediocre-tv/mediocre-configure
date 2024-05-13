190f05791689a584b10f6a1ab610d234bef00cee
Oliver Broomhall <obroomhall@gmail.com>
2024-05-13 12:51:17 +0100

    Run with HTTPS if certs exist

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,29 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import fs from "fs";
 
+function developmentConfig() {
+  let https;
+  try {
+    https = {
+      key: fs.readFileSync("./.certs/localhost/key.pem"),
+      cert: fs.readFileSync("./.certs/localhost/cert.pem"),
+    };
+  } catch (error) {
+    console.error(
+      "Could not load TLS certificates, falling back to HTTP.\n",
+      error,
+    );
+  }
+
+  return {
+    server: {
+      https,
+    },
+  };
+}
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  ...(mode === "development" && {
-    server: {
-      https: {
-        key: fs.readFileSync("../.certs/localhost/key.pem"),
-        cert: fs.readFileSync("../.certs/localhost/cert.pem"),
-      },
-    },
-  }),
+  ...(mode === "development" && developmentConfig()),
   plugins: [react()],
 }));
