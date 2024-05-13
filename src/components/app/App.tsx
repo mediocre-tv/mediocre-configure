2d034783001b9129da3da650f324f02be7adf7a9
Oliver Broomhall <obroomhall@gmail.com>
2024-05-13 21:16:23 +0100

    Replace App.module.css with MUI Stack

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,4 +1,3 @@
-import styles from "./App.module.css";
 import { useState } from "react";
 import GrpcConfig from "../grpc/GrpcConfig.tsx";
 import AppProviders from "../app-providers/AppProviders.tsx";
@@ -9,13 +8,14 @@ import "@fontsource/roboto/700.css";
 import GrpcProvider from "../grpc/GrpcProvider.tsx";
 import { GrpcContextProps } from "../grpc/GrpcContext.tsx";
 import RegionEditor from "../region-editor/RegionEditor.tsx";
+import { Stack } from "@mui/material";
 
 function App() {
   const [grpcContext, setGrpcContext] = useState<GrpcContextProps | null>(null);
 
   return (
     <AppProviders>
-      <div className={styles.container}>
+      <Stack height="100%" justifyContent="center" alignItems="center">
         {grpcContext ? (
           <GrpcProvider context={grpcContext}>
             <RegionEditor />
@@ -23,7 +23,7 @@ function App() {
         ) : (
           <GrpcConfig context={grpcContext} setContext={setGrpcContext} />
         )}
-      </div>
+      </Stack>
     </AppProviders>
   );
 }
